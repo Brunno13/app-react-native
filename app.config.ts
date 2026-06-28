70f01d047d512924ef2b0c48fce5bd61458ca02d
Brunno <brunnosantos@mobilesaude.com.br>
2026-06-28 00:00:00 -0300

    change app version

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: IS_PROD ? 'App Bun' : 'App Bun (Staging)',
   slug: 'app-react-native',
   scheme: 'app-react-native',
-  version: '1.0.3',
+  version: '1.0.4',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
